b52236b2676a20b661c2ffd10fa5f1d544228b0f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-12-28 08:39:26 +0700

    composite build fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,30 @@
 rootProject.name = "moko-libs"
 
-includeBuild("graphics")
-includeBuild("parcelize")
-includeBuild("resources")
-includeBuild("mvvm")
-//includeBuild("units")
-includeBuild("fields")
-includeBuild("permissions")
-includeBuild("media")
+fun org.gradle.api.initialization.ConfigurableIncludedBuild.substituteMpp(name: String = this.name) {
+    dependencySubstitution {
+        listOf(
+            name,
+            "$name-iosx64",
+            "$name-iosarm64"
+        ).forEach {
+            substitute(module("dev.icerock.moko:$it"))
+                .because("multiplatform artifacts")
+                .with(project(":$name"))
+        }
+    }
+}
+
+includeBuild("graphics") { substituteMpp() }
+includeBuild("parcelize") { substituteMpp() }
+includeBuild("resources/resources-gradle-plugin")
+includeBuild("resources") { substituteMpp() }
+includeBuild("mvvm") { substituteMpp() }
+includeBuild("errors") { substituteMpp() }
+includeBuild("network/network-gradle-plugin")
+includeBuild("network") { substituteMpp() }
+includeBuild("units/units-gradle-plugin")
+includeBuild("units") { substituteMpp() }
+includeBuild("permissions") { substituteMpp() }
+includeBuild("media") { substituteMpp() }
+includeBuild("fields") { substituteMpp() }
+includeBuild("template")
