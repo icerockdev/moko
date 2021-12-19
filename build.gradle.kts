0c9241fd94e95daecfda95c863958bd171596541
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-12-19 18:29:15 +0700

    test composite at 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,61 @@
-
-val projectPathMap: Map<String, String> = mapOf(
-    "graphics" to "graphics",
-    "parcelize" to "parcelize",
-    "resources" to "resources",
-    "resources-gradle-plugin" to "resources-generator",
-    "mvvm" to "mvvm",
-    "template" to "mpp-library",
-    "template" to "android-app"
-)
-
-fun createProxyTask(taskName: String) {
-    tasks.create(taskName) {
-        group = "build"
-
-        projectPathMap.forEach { (build, project) ->
-            val includedBuild = gradle.includedBuild(build)
-            dependsOn(includedBuild.task(":$project:$taskName"))
-        }
-    }
-}
-
-createProxyTask("build")
-createProxyTask("clean")
-
-data class TaskDesc(
-    val includedBuild: String,
-    val projectPath: String = includedBuild,
-    val tasks: List<String> = listOf(
-        "assemble",
-        "assembleDebug",
-        "assembleRelease",
-        "compileKotlinIosArm64",
-        "compileKotlinIosX64",
-        "build",
-        "clean",
-        "dependencies"
-    )
-)
-
-listOf(
-    TaskDesc(includedBuild = "graphics"),
-    TaskDesc(includedBuild = "parcelize"),
-    TaskDesc(includedBuild = "resources"),
-    TaskDesc(includedBuild = "mvvm"),
-    TaskDesc(includedBuild = "errors"),
-    TaskDesc(includedBuild = "network"),
-    TaskDesc(includedBuild = "template", projectPath = "mpp-library")
-).forEach { taskDesc ->
-    taskDesc.tasks.forEach { taskName ->
-        val includedBuildName = taskDesc.includedBuild.capitalize()
-        val projectName = taskDesc.projectPath.capitalize()
-
-        tasks.create("$taskName$includedBuildName$projectName") {
-            group = taskDesc.includedBuild
-
-            val task = gradle.includedBuild(taskDesc.includedBuild).task(":${taskDesc.projectPath}:$taskName")
-            dependsOn(task)
-        }
-    }
-}
+//
+//val projectPathMap: Map<String, String> = mapOf(
+//    "graphics" to "graphics",
+//    "parcelize" to "parcelize",
+//    "resources" to "resources",
+//    "resources-generator" to "resources-generator",
+////    "mvvm" to "mvvm",
+////    "template" to "mpp-library",
+////    "template" to "android-app"
+//)
+//
+//fun createProxyTask(taskName: String) {
+//    tasks.create(taskName) {
+//        group = "build"
+//
+//        projectPathMap.forEach { (build, project) ->
+//            val includedBuild = gradle.includedBuild(build)
+//            dependsOn(includedBuild.task(":$project:$taskName"))
+//        }
+//    }
+//}
+//
+//createProxyTask("build")
+//createProxyTask("clean")
+//
+//data class TaskDesc(
+//    val includedBuild: String,
+//    val projectPath: String = includedBuild,
+//    val tasks: List<String> = listOf(
+//        "assemble",
+//        "assembleDebug",
+//        "assembleRelease",
+//        "compileKotlinIosArm64",
+//        "compileKotlinIosX64",
+//        "build",
+//        "clean",
+//        "dependencies"
+//    )
+//)
+//
+//listOf(
+//    TaskDesc(includedBuild = "graphics"),
+//    TaskDesc(includedBuild = "parcelize"),
+//    TaskDesc(includedBuild = "resources"),
+//    TaskDesc(includedBuild = "mvvm"),
+//    TaskDesc(includedBuild = "errors"),
+//    TaskDesc(includedBuild = "network"),
+//    TaskDesc(includedBuild = "template", projectPath = "mpp-library")
+//).forEach { taskDesc ->
+//    taskDesc.tasks.forEach { taskName ->
+//        val includedBuildName = taskDesc.includedBuild.capitalize()
+//        val projectName = taskDesc.projectPath.capitalize()
+//
+//        tasks.create("$taskName$includedBuildName$projectName") {
+//            group = taskDesc.includedBuild
+//
+//            val task = gradle.includedBuild(taskDesc.includedBuild).task(":${taskDesc.projectPath}:$taskName")
+//            dependsOn(task)
+//        }
+//    }
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,33 +5,33 @@ plugins {
 }
 
 fun org.gradle.api.initialization.ConfigurableIncludedBuild.substituteMpp(name: String = this.name) {
-    dependencySubstitution {
-        listOf(
-            name,
-            "$name-iosx64",
-            "$name-iosarm64"
-        ).forEach {
-            substitute(module("dev.icerock.moko:$it"))
-                .because("multiplatform artifacts")
-                .with(project(":$name"))
-        }
-    }
+//    dependencySubstitution {
+//        listOf(
+//            name,
+//            "$name-iosx64",
+//            "$name-iosarm64"
+//        ).forEach {
+//            substitute(module("dev.icerock.moko:$it"))
+//                .because("multiplatform artifacts")
+//                .with(project(":$name"))
+//        }
+//    }
 }
 
-includeBuild("graphics") { substituteMpp() }
-includeBuild("parcelize") { substituteMpp() }
-includeBuild("resources/resources-gradle-plugin")
-includeBuild("resources") { substituteMpp() }
-includeBuild("mvvm") { substituteMpp() }
-includeBuild("errors") { substituteMpp() }
-includeBuild("network/network-gradle-plugin")
-includeBuild("network") { substituteMpp() }
-includeBuild("units/units-gradle-plugin")
-includeBuild("units") { substituteMpp() }
-includeBuild("permissions") { substituteMpp() }
-includeBuild("media") { substituteMpp() }
-includeBuild("fields") { substituteMpp() }
-includeBuild("template")
+includeBuild("moko-graphics") { substituteMpp() }
+includeBuild("moko-parcelize") { substituteMpp() }
+includeBuild("moko-resources/resources-generator")
+includeBuild("moko-resources") { substituteMpp() }
+//includeBuild("mvvm") { substituteMpp() }
+//includeBuild("errors") { substituteMpp() }
+//includeBuild("network/network-gradle-plugin")
+//includeBuild("network") { substituteMpp() }
+//includeBuild("units/units-gradle-plugin")
+//includeBuild("units") { substituteMpp() }
+//includeBuild("permissions") { substituteMpp() }
+//includeBuild("media") { substituteMpp() }
+//includeBuild("fields") { substituteMpp() }
+//includeBuild("template")
 
 gradleEnterprise {
     buildScan {
